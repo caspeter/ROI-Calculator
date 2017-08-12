4c03016f2344af5c34faa4f4cac65b403497a288
Cassie Peterson <cas.j.peters@gmail.com>
2017-08-12 14:17:16 -0600

    All calculations are at base functionality. Need to add odd use cases

--- revenue/revenue.controller.js
+++ revenue/revenue.controller.js
@@ -21,43 +21,54 @@
       vm.newExpense = {};
       vm.totalOneTimeRevenue = 0;
       vm.totalMonthlyRevenue = 0;
+      vm.totalRevenue = 0;
       vm.totalOneTimeExpense = 0;
       vm.totalMonthlyExpense = 0;
+      vm.totalExpense = 0;
+      vm.monthlyContributionProfit = 0;
+      vm.contributionProfit = 0;
+      vm.contributionMargin = 0;
+      vm.capitalROI = 0;
       vm.allRev = [
         {
-          description: 'text',
-          oneTime: 23,
-          monthly: 45
+          description: 'text1',
+          oneTime: 100,
+          monthly: 50
         }, {
-          description: 'text',
-          oneTime: 23,
-          monthly: 45
+          description: 'text2',
+          oneTime: 50,
+          monthly: 25
+        } ,{
+          description: 'text3',
+          oneTime: 25,
+          monthly: 85
         }
       ];
       vm.allExpense = [
         {
         description: 'expense1',
-        oneTime: 20,
-        monthly: 40
+        oneTime: 500,
+        monthly: 20
       }, {
         description: 'expense2',
-        oneTime: 20,
+        oneTime: 200,
         monthly: 40
       }];
+      recalculate();
     }
 
     function submitNewRevenue() {
       vm.allRev.push(vm.newRev);
       console.log(vm.allRev);
       vm.newRev = {};
-      recalculateTotals();
+      recalculate();
     }
 
     function submitNewExpense() {
       vm.allExpense.push(vm.newExpense);
       console.log(vm.allExpense);
       vm.newExpense = {};
-      recalculateTotals();
+      recalculate();
     }
 
 
@@ -69,11 +80,39 @@
       return total;
     };
 
-    function recalculateTotals() {
+    function calculateMonthlyContributionProfit(){
+      console.log('run calc monthly contr');
+      //Monthly Contribution Profit = Monthly Revenue – Monthly Expenses
+      console.log(' calculateMonthlyContributionProfit ', vm.totalMonthlyRevenue - vm.totalMonthlyExpense);
+      return vm.totalMonthlyRevenue - vm.totalMonthlyExpense;
+    }
+
+    function totalContributionProfit() {
+      //•	Total Contribution Profit = Total Revenue – Total Expenses
+      return vm.totalRevenue - vm.totalExpense;
+    }
+
+    function calculateContributionMargin(){
+      //Contribution Margin = Total Contribution Profit / Total Revenue
+      return vm.contributionProfit/vm.totalRevenue * 100;
+    }
+
+    function calculateCapitalROI() {
+      //Capital ROI (Months) = (One-Time Expenses – One-Time Revenue) / Monthly Contribution Profit
+      return (vm.totalOneTimeExpense - vm.totalOneTimeRevenue) / vm.monthlyContributionProfit;
+    }
+
+    function recalculate() {
       vm.totalOneTimeExpense = calculateTotals(vm.allExpense, 'oneTime');
       vm.totalMonthlyExpense = calculateTotals(vm.allExpense, 'monthly');
       vm.totalOneTimeRevenue = calculateTotals(vm.allRev, 'oneTime');
       vm.totalMonthlyRevenue = calculateTotals(vm.allRev, 'monthly');
+      vm.totalRevenue = (vm.totalMonthlyRevenue*12) + vm.totalOneTimeRevenue;
+      vm.totalExpense = (vm.totalMonthlyExpense*12) + vm.totalOneTimeExpense;
+      vm.monthlyContributionProfit = calculateMonthlyContributionProfit();
+      vm.contributionProfit = totalContributionProfit();
+      vm.contributionMargin = Math.round(calculateContributionMargin() * 100)/100;
+      vm.capitalROI = Math.round(calculateCapitalROI()*10)/10;
     }
 
   }
