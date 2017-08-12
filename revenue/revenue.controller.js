fa28de2868797f88871a0285c8b9d820e55f147f
Cassie Peterson <cas.j.peters@gmail.com>
2017-08-12 12:07:02 -0600

    Forms are complete and adding items, no math is happening yet

--- revenue/revenue.controller.js
+++ revenue/revenue.controller.js
@@ -1,29 +1,58 @@
 (function() {
 
-    'use strict';
+  'use strict';
 
-    angular
-        .module("app")
-        .component("revenue", {
-            controller: RevenueController,
-            templateUrl: './revenue/revenue.template.html'
-        })
+  angular.module("app").component("revenue", {
+    controller: RevenueController,
+    templateUrl: './revenue/revenue.template.html'
+  })
 
-    function RevenueController() {
-        var vm = this;
+  function RevenueController() {
+    var vm = this;
 
-        vm.$onInit = onInit;
-        vm.submitNewRevenue = submitNewRevenue;
+    vm.$onInit = onInit;
+    vm.submitNewRevenue = submitNewRevenue;
+    vm.submitNewExpense = submitNewExpense;
 
-        function onInit() {
-          console.log('Loaded');
-          vm.revenues= [];
-          vm.newRev = {};
+    function onInit() {
+      console.log('Loaded');
+      vm.revenues = [];
+      vm.newRev = {};
+      vm.newExpense = {};
+      vm.allRev = [
+        {
+          description: 'text',
+          oneTime: 23,
+          monthly: 45
+        }, {
+          description: 'text',
+          oneTime: 23,
+          monthly: 45
         }
-
-        function submitNewRevenue(){
-          console.log(vm.newRev);
-        }
-
+      ];
+      vm.allExpense = [
+        {
+        description: 'expense1',
+        oneTime: 20,
+        monthly: 40
+      }, {
+        description: 'expense2',
+        oneTime: 20,
+        monthly: 40
+      }];
     }
+
+    function submitNewRevenue() {
+      vm.allRev.push(vm.newRev);
+      console.log(vm.allRev);
+      vm.newRev = {};
+    }
+
+    function submitNewExpense() {
+      vm.allExpense.push(vm.newExpense);
+      console.log(vm.allExpense);
+      vm.newExpense = {};
+    }
+
+  }
 })();
